7911433446b6373e2cd0eb697671485076e701f7
Mark "Justin du Coeur" Waks <github@waks.org>
2021-10-10 12:03:12 -0400

    Display the local address
    This way, when we scale the cluster to several nodes with docker-compose, we can show that we are load-balancing among them.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,12 +2,14 @@ package controllers
 
 import play.api.mvc.{Action, Controller, EssentialAction}
 
+import java.net.InetAddress
 import javax.inject.{Inject, Provider}
 
 class HomeController @Inject() (val appProv: Provider[play.api.Application]) extends Controller {
   implicit lazy val app = appProv.get
 
   def hello(): EssentialAction = Action { implicit request =>
-    Ok("Look, you called hello()")
+    val localAddr = InetAddress.getLocalHost.getHostAddress
+    Ok(s"Look, you called hello() on $localAddr")
   }
 }
