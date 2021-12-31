8d6cb47385320e65694a7f99a96f8b063e0bf66f
Mark "Justin du Coeur" Waks <github@waks.org>
2021-12-31 11:48:43 -0500

    Move the application secret to AWS Secrets Manager

--- app/querki/system/QuerkiApplicationLoader.scala
+++ app/querki/system/QuerkiApplicationLoader.scala
@@ -1,12 +1,17 @@
 package querki.system
 
 import play.api.inject.guice.{GuiceApplicationBuilder, GuiceApplicationLoader}
-import play.api.{Application, ApplicationLoader}
+import play.api.{Application, ApplicationLoader, Configuration}
 
 class QuerkiApplicationLoader extends ApplicationLoader {
 
   def load(context: ApplicationLoader.Context): Application = {
-    val newConfig = context.initialConfiguration
+    val appSecret = Secrets.getSecret("app_secret")
+    // Note that play.crypto.secret changes name in newer versions of Play:
+    val secretsConfig: Configuration = Configuration("play.crypto.secret" -> appSecret)
+
+    // Note that ++ is replaced (and flipped in order) by withFallback() in newer versions of Play:
+    val newConfig = context.initialConfiguration ++ secretsConfig
     val newContext = context.copy(initialConfiguration = newConfig)
     val builder = new GuiceApplicationBuilder()
     val app = (new GuiceApplicationLoader(builder)).load(newContext)
