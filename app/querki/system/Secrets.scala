59afe3e7047410b81bf7b8136d622e26faaabc3c
Mark "Justin du Coeur" Waks <github@waks.org>
2022-05-30 12:53:43 -0400

    Un-hardcode the endpoint for the Secrets Manager
    In local development, we need to point this at Localstack, but in the production environment, we want to let AWS just use its default.

--- app/querki/system/Secrets.scala
+++ app/querki/system/Secrets.scala
@@ -11,21 +11,34 @@ import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest
  * *extremely* early -- we need to be able to access secrets in order to build the Play application itself (to
  * get at the app secret). So see if the Ecology is built early enough. For purposes of this example, I'm not
  * worrying about it.
+ *
+ * Note that this code is loosely adapted from the suggested Java code example provided by AWS Secrets Manager
+ * when I created the POC secret.
  */
 object Secrets {
 
-  lazy val secretsManager =
-    AWSSecretsManagerClientBuilder
+  val secretName = "querki/poc/secrets"
+  val region = "us-east-1"
+
+  // Grungy but practical way to get the endpoint from the environment if it is defined.
+  // In theory, this should come via application.conf; in practice, that isn't working for me. Why not?
+  def secretsEndpoint: Option[String] = sys.env.get("SECRETS_ENDPOINT")
+
+  lazy val secretsManager = {
+    val base = AWSSecretsManagerClientBuilder
       .standard()
-      // "host.docker.internal" is how we break out of our Docker container, to use localhost for localstack.
-      // Port 4566 is localstack.
-      // Need to explicitly use http, or it will try to use https and get confused.
-      // TODO: need to make this configurable, so that it can work for real when deployed:
-      .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
-        "http://host.docker.internal:4566",
-        "us-east-1"
+    val configured = secretsEndpoint.map { endpoint =>
+      // If there is a configured endpoint, we're in local development, and need to point to that:
+      base.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+        endpoint,
+        region
       ))
-      .build()
+    }.getOrElse {
+      // Actually in AWS, so we don't need to specify the endpoint (?):
+      base.withRegion(region)
+    }
+    configured.build()
+  }
 
   // Yes, this is horrible and Java-y and blocking and mutating. But it's throwaway test setup code.
   def getSecret(name: String): String = {
